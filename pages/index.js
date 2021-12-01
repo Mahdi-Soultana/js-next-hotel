c06bdc3eef9c5d1cf0e79c0e5ecdc3983bc7ab45
Mahdi-Soultana <soultana.mahdi24@gmail.com>
2021-11-30 17:52:03 -0800

    redux react-query

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,22 @@
-import Head from "next/head";
-import Image from "next/image";
-import styles from "../styles/Home.module.css";
 import Layout from "../components/Layout/index";
 import Home from "../components/Home";
+
+import { QueryCache } from "react-query";
+import { dehydrate } from "react-query/hydration";
+
+export async function getStaticProps() {
+  const queryCache = new QueryCache();
+
+  await queryCache.prefetchQuery("posts", getPosts);
+
+  return {
+    props: {
+      dehydratedState: dehydrate(queryCache),
+    },
+  };
+}
 export default function Index() {
+  const { data } = useQuery("posts", getPosts);
   return (
     <Layout title="Book the Best Room for your Holiday">
       <Home />
